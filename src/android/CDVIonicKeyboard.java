e0ce094de2fd29f29ba39a7803787d1285802331
Albert Gasset <albert.gasset@moodle.com>
2025-07-23 14:40:54 +0200

    Port Android implementation from Capacitor plugin

--- src/android/CDVIonicKeyboard.java
+++ src/android/CDVIonicKeyboard.java
@@ -9,26 +9,11 @@ import org.apache.cordova.PluginResult.Status;
 import org.json.JSONArray;
 import org.json.JSONException;
 
-import android.content.Context;
-import android.graphics.Rect;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.view.ViewTreeObserver;
-import android.view.ViewTreeObserver.OnGlobalLayoutListener;
-import android.view.inputmethod.InputMethodManager;
-
-// import additionally required classes for calculating screen height
-import android.view.Display;
-import android.graphics.Point;
-import android.os.Build;
-import android.widget.FrameLayout;
+import android.os.Handler;
+import android.os.Looper;
 
 public class CDVIonicKeyboard extends CordovaPlugin {
-    private OnGlobalLayoutListener list;
-    private View rootView;
-    private View mChildOfContent;
-    private int usableHeightPrevious;
-    private FrameLayout.LayoutParams frameLayoutParams;
+    private Keyboard implementation;
 
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
@@ -36,122 +21,64 @@ public class CDVIonicKeyboard extends CordovaPlugin {
 
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if ("hide".equals(action)) {
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    //http://stackoverflow.com/a/7696791/1091751
-                    InputMethodManager inputManager = (InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
-                    View v = cordova.getActivity().getCurrentFocus();
-
-                    if (v == null) {
-                        callbackContext.error("No current focus");
+            cordova.getThreadPool().execute(
+                () -> {
+                   if (!implementation.hide()) {
+                        callbackContext.error("Can't close keyboard, not currently focused");
                     } else {
-                        inputManager.hideSoftInputFromWindow(v.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
                         callbackContext.success(); // Thread-safe.
                     }
                 }
-            });
+            );
             return true;
         }
         if ("show".equals(action)) {
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    ((InputMethodManager) cordova.getActivity().getSystemService(Context.INPUT_METHOD_SERVICE)).toggleSoftInput(0, InputMethodManager.HIDE_IMPLICIT_ONLY);
-                    callbackContext.success(); // Thread-safe.
-                }
-            });
+            cordova.getThreadPool().execute(
+                () ->
+                    new Handler(Looper.getMainLooper())
+                        .postDelayed(
+                            () -> {
+                                implementation.show();
+                                callbackContext.success(); // Thread-safe.
+                            },
+                            350
+                        )
+            );
             return true;
         }
         if ("init".equals(action)) {
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                	//calculate density-independent pixels (dp)
-                    //http://developer.android.com/guide/practices/screens_support.html
-                    DisplayMetrics dm = new DisplayMetrics();
-                    cordova.getActivity().getWindowManager().getDefaultDisplay().getMetrics(dm);
-                    final float density = dm.density;
-
-                    //http://stackoverflow.com/a/4737265/1091751 detect if keyboard is showing
-                    FrameLayout content = (FrameLayout) cordova.getActivity().findViewById(android.R.id.content);
-                    rootView = content.getRootView();
-                    list = new OnGlobalLayoutListener() {
-                        int previousHeightDiff = 0;
-                        @Override
-                        public void onGlobalLayout() {
-                            boolean resize = preferences.getBoolean("resizeOnFullScreen", false);
-                            if (resize) {
-                                possiblyResizeChildOfContent();
-                            }
-                            Rect r = new Rect();
-                            //r will be populated with the coordinates of your view that area still visible.
-                            rootView.getWindowVisibleDisplayFrame(r);
-
+            cordova.getThreadPool().execute(
+                () -> {
+                    boolean resizeOnFullScreen = preferences.getBoolean("resizeOnFullScreen", false);
+                    implementation = new Keyboard(cordova.getActivity(), resizeOnFullScreen);
+                    implementation.setKeyboardEventListener(
+                        (String event, int size) -> {
                             PluginResult result;
-
-                            // cache properties for later use
-                            int rootViewHeight = rootView.getRootView().getHeight();
-                            int resultBottom = r.bottom;
-
-                            // calculate screen height differently for android versions >= 21: Lollipop 5.x, Marshmallow 6.x
-                            //http://stackoverflow.com/a/29257533/3642890 beware of nexus 5
-                            int screenHeight;
-
-                            if (Build.VERSION.SDK_INT >= 21) {
-                                Display display = cordova.getActivity().getWindowManager().getDefaultDisplay();
-                                Point size = new Point();
-                                display.getSize(size);
-                                screenHeight = size.y;
-                            } else {
-                                screenHeight = rootViewHeight;
-                            }
-
-                            int heightDiff = screenHeight - resultBottom;
-
-                            int pixelHeightDiff = (int)(heightDiff / density);
-                            if (pixelHeightDiff > 100 && pixelHeightDiff != previousHeightDiff) { // if more than 100 pixels, its probably a keyboard...
-                                String msg = "S" + Integer.toString(pixelHeightDiff);
-                                result = new PluginResult(PluginResult.Status.OK, msg);
-                                result.setKeepCallback(true);
-                                callbackContext.sendPluginResult(result);
-                            }
-                            else if ( pixelHeightDiff != previousHeightDiff && ( previousHeightDiff - pixelHeightDiff ) > 100 ){
-                            	String msg = "H";
-                                result = new PluginResult(PluginResult.Status.OK, msg);
-                                result.setKeepCallback(true);
-                                callbackContext.sendPluginResult(result);
-                            }
-                            previousHeightDiff = pixelHeightDiff;
-                        }
-
-                        private void possiblyResizeChildOfContent() {
-                            int usableHeightNow = computeUsableHeight();
-                            if (usableHeightNow != usableHeightPrevious) {
-                                int usableHeightSansKeyboard = mChildOfContent.getRootView().getHeight();
-                                int heightDifference = usableHeightSansKeyboard - usableHeightNow;
-                                if (heightDifference > (usableHeightSansKeyboard/4)) {
-                                    frameLayoutParams.height = usableHeightSansKeyboard - heightDifference;
-                                } else {
-                                    frameLayoutParams.height = usableHeightSansKeyboard;
-                                }
-                                mChildOfContent.requestLayout();
-                                usableHeightPrevious = usableHeightNow;
+                            String msg;
+                            switch (event) {
+                                case Keyboard.EVENT_KB_WILL_SHOW:
+                                case Keyboard.EVENT_KB_DID_SHOW:
+                                    msg = "S" + Integer.toString(size);
+                                    result = new PluginResult(PluginResult.Status.OK, msg);
+                                    result.setKeepCallback(true);
+                                    callbackContext.sendPluginResult(result);
+                                    break;
+                                case Keyboard.EVENT_KB_WILL_HIDE:
+                                case Keyboard.EVENT_KB_DID_HIDE:
+                                    msg = "H";
+                                    result = new PluginResult(PluginResult.Status.OK, msg);
+                                    result.setKeepCallback(true);
+                                    callbackContext.sendPluginResult(result);
+                                    break;
                             }
                         }
+                    );
 
-                        private int computeUsableHeight() {
-                            Rect r = new Rect();
-                            mChildOfContent.getWindowVisibleDisplayFrame(r);
-                            return (r.bottom - r.top);
-                        }
-                    };
-
-                    mChildOfContent = content.getChildAt(0);
-                    rootView.getViewTreeObserver().addOnGlobalLayoutListener(list);
-                    frameLayoutParams = (FrameLayout.LayoutParams) mChildOfContent.getLayoutParams();
                     PluginResult dataResult = new PluginResult(PluginResult.Status.OK);
                     dataResult.setKeepCallback(true);
                     callbackContext.sendPluginResult(dataResult);
                 }
-            });
+            );
             return true;
         }
         return false;  // Returning false results in a "MethodNotFound" error.
@@ -159,7 +86,7 @@ public class CDVIonicKeyboard extends CordovaPlugin {
 
     @Override
     public void onDestroy() {
-        rootView.getViewTreeObserver().removeOnGlobalLayoutListener(list);
+        implementation.setKeyboardEventListener(null);
     }
 
 }
